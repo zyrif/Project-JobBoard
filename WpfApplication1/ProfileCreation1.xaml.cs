103eaa6cea75cd0fbf303d8ab1221be7b54ff1e6
dotFour <rubaiyat9@gmail.com>
2016-12-01 21:21:58 +0600

    Profile Creation Step One

---
+++ WpfApplication1/ProfileCreation1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ProfileCreation1 : Window
+    {
+        public ProfileCreation1()
+        {
+            InitializeComponent();
+        }
+
+        private void WelcomeEnter_Click(object sender, RoutedEventArgs e)
+        {
+            LoginRegister lr = new LoginRegister();
+            lr.Show();
+            this.Hide();
+        }
+
+        private void WindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WindowMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
