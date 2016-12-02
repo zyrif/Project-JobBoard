74f28d0a2111a43599847841e6cb9d0efc438420
dotFour <rubaiyat9@gmail.com>
2016-12-02 22:59:31 +0600

    Added Recruiter Window + minor fixes

--- WpfApplication1/WelcomeWindow.xaml.cs
+++ WpfApplication1/WelcomeWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace WpfApplication1
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Welcome : Window
     {
-        public MainWindow()
+        public Welcome()
         {
             InitializeComponent();
         }
