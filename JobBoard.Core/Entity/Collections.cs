9d4f725af04b06934d9c33384d17303ede991cb1
sh0v0n <shvnbasak@outlook.com>
2016-12-08 23:08:44 +0600

    Entity classes

--- JobBoard.Core/Entity/Collections.cs
+++ JobBoard.Core/Entity/Collections.cs
@@ -8,38 +8,8 @@ namespace JobBoard.Core.Entity
 {
     class Collections
     {
-         List<string> experience=new List<string>();
-         List<string> education = new List<string>();
-         List<string> postedjob = new List<string>();
-
-         public void setExperience(string skill)
-         {
-             experience.Add(skill);
-         }
-         public List<string> getExperience()
-         {
-             return experience;
-         }
-
-         
-        public void setEducation(string edu)
-         {
-             education.Add(edu);
-         }
-         public List<string> getExperience()
-         {
-             return experience;
-         }
-
-        
-        public void setPostJob(string postjob)
-         {
-             postedjob.Add(postjob);
-         }
-         public List<string> getPostJob()
-         {
-             return postedjob;
-         }
-
+        public List<Experience> experience=new List<Experience>();
+        public List<Experience> education = new List<Experience>();
+        public List<Experience> postedjob = new List<Experience>();
     }
 }
