6edfbeb976271a536a0cdba5a9b4e48bdce9c037
dotFour <rubaiyat9@gmail.com>
2016-12-09 22:13:49 +0600

    AddVanacy window completed

---
+++ JobBoard.WpfApplication/AddVacancy.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JobBoard.WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for AddVacancy.xaml
+    /// </summary>
+    public partial class AddVacancy : Window
+    {
+        public AddVacancy()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WindowMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
