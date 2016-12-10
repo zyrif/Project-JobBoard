1c8f278023551a773cc3a4a4ddccc31219aaa1b4
sh0v0n <shvnbasak@outlook.com>
2016-12-10 17:28:47 +0600

    Data

--- JobBoard.Data/DBReadWrite.cs
+++ JobBoard.Data/DBReadWrite.cs
@@ -4,23 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Data;
+using System.Windows.Forms;
 
 namespace JobBoard.Data
 {
     public class DBReadWrite
     {
-        public static SqlConnection sqlConnection;
+        public static SqlConnection connection;
         public DBReadWrite()
         {
-            if(sqlConnection == null)
+            if(connection == null)
                 this.createConnection();
         }
 
-        public static DataTable selectQuery(string query)
+        public void insertQuery(string query)
+        {
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+        }
+
+        public DataTable selectQuery(string query)
         {
             //Reading Login Data and putting them in a Data Table
-            SqlCommand sqlCommand = new SqlCommand(query, DBReadWrite.sqlConnection);
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
             DataTable dataTable = new DataTable();
             sqlDataAdapter.Fill(dataTable);
@@ -31,13 +38,14 @@ namespace JobBoard.Data
         //To create Connection with DataBase
         public void createConnection()
         {
-            sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\SBS\Documents\JobBoard.mdf;Integrated Security=True;Connect Timeout=30");
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\SBS\Documents\JobBoard.mdf;Integrated Security=True;Connect Timeout=30");
+            //connection = new MySqlConnection(@"SERVER=127.0.0.1;PORT=3306;UID=JBapp;PASSWORD=jason6;DATABASE=dbJobBoard");
         }
 
         //To close the connection
         void closeConnection()
         {
-            sqlConnection.Close();
+            connection.Close();
         }
     }
 }
--- JobBoard.Data/UserInfo.cs
+++ JobBoard.Data/UserInfo.cs
@@ -10,51 +10,63 @@ namespace JobBoard.Data
 {
     public class UserInfo
     {
+        DBReadWrite dbReadWrite = new DBReadWrite();
         DataTable dataTable;
-        public int getSystemId(string userName, string passWord)
+        public bool getUser(string userName, string passWord)
         {
             string query = "select * from LoginInfo where username ='" + userName.Trim() + "' and password='" + passWord.Trim() + "'";
-            dataTable = DBReadWrite.selectQuery(query);
+            dataTable = dbReadWrite.selectQuery(query);
 
             if (dataTable.Rows.Count == 1)
-                return Convert.ToInt32(dataTable.Rows[0]["SystemId"]);
-            else
-                return -1;
+                return true;
+            
+            return false;
         }
 
-        public int getSystemId(string userName)
+        public bool getUser(string userName)
         {
             string query = "select * from LoginInfo where username ='" + userName.Trim()+"'";
-            dataTable = DBReadWrite.selectQuery(query);
+            dataTable = dbReadWrite.selectQuery(query);
 
             if (dataTable.Rows.Count == 1)
-                return Convert.ToInt32(dataTable.Rows[0]["SystemId"]);
+                return true;
+
+            return false;
+        }
+
+        public DataTable getUserInfo(string userName)
+        {
+            string query = "select * from UserDetail where UserName ='" + userName + "'";
+            dataTable = dbReadWrite.selectQuery(query);
+
+
+            if (dataTable.Rows.Count > 0)
+                System.Windows.Forms.MessageBox.Show("1");
             else
-                return -1;
+                System.Windows.Forms.MessageBox.Show("2");
+            return dataTable;
         }
 
-        public DataTable getUserInfo(int systemId)
+        public DataTable getBirthday(string userName)
         {
-            string query = "select * from UserDetail where SystemId =" + systemId;
-            dataTable = DBReadWrite.selectQuery(query);
+            string query = "select * from JobSeeker where UserName ='" + userName + "'";
+            dataTable = dbReadWrite.selectQuery(query);
 
             return dataTable;
         }
 
-        public DataTable getBirthday(int systemId)
+        public DataTable getSkill(string userName)
         {
-            string query = "select * from JobSeeker where SystemId =" + systemId;
-            dataTable = DBReadWrite.selectQuery(query);
+            string query = "select * from Skill where UserName ='" + userName+"'";
+            dataTable = dbReadWrite.selectQuery(query);
 
             return dataTable;
         }
 
-        public DataTable getSkill(int systemId)
+        public void createUser(string userName, string passWord)
         {
-            string query = "select * from Skill where SystemId =" + systemId;
-            dataTable = DBReadWrite.selectQuery(query);
-
-            return dataTable;
+            string query = "INSERT INTO LoginInfo VALUES('" + userName.Trim() + "','" + passWord.Trim() + "')";
+            dataTable = dbReadWrite.selectQuery(query);
         }
     }
 }
