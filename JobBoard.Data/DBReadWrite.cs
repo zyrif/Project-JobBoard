d53ffdd453d9425a693f1d6f5348b8bbc9ea65fd
dotFour <rubaiyat9@gmail.com>
2016-12-12 22:01:13 +0600

    Added editable combobox in job seeker registration

--- JobBoard.Data/DBReadWrite.cs
+++ JobBoard.Data/DBReadWrite.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-using MySql.Data.MySqlClient;
+//using MySql.Data.MySqlClient;
 using System.Data;
 using System.Windows.Forms;
 
