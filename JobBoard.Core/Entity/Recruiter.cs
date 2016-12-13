fa2924bd27a97029d37614ec132034b65533b6e7
sh0v0n <shvnbasak@outlook.com>
2016-12-13 19:51:07 +0600

    Data initialization for logged user

--- JobBoard.Core/Entity/Recruiter.cs
+++ JobBoard.Core/Entity/Recruiter.cs
@@ -9,12 +9,14 @@ namespace JobBoard.Core
     class Recruiter:User
     {
         public string JobPosition { get; set; }
-        public string Employer { get; set; }
+        public uint CompanyId { get; set; }
 
-        public Recruiter(string userName, string userPass, string firstName, string lastName, string email, string phoneNumber, string jobPosition, string employer):base(userName, userPass, firstName, lastName, email, phoneNumber)
+        public Recruiter() { }
+
+        public Recruiter(string userName, string userPass, string firstName, string lastName, string email, string phoneNumber, string jobPosition, uint companyId):base(userName, userPass, firstName, lastName, email, phoneNumber)
         {
             this.JobPosition = jobPosition;
-            this.Employer = employer;
+            this.CompanyId = companyId;
         }
     } 
 }
