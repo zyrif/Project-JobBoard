afe2da44dd6b01a25ef1c83d7047616c79fc88f4
sh0v0n <shvnbasak@outlook.com>
2017-01-01 16:21:02 +0600

    employer reg window problem fix

--- JobBoard.WpfApplication/EmployerRegistrationWindow.xaml.cs
+++ JobBoard.WpfApplication/EmployerRegistrationWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using JobBoard.Core;
+using JobBoard.Core.Control;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,12 +21,14 @@ namespace JobBoard.WpfApplication
     /// </summary>
     public partial class EmployerRegistration : Window
     {
+        IEHPatterns iehp = IEHPatterns.getInstance();
         LoginRegistrationControl lrControl = LoginRegistrationControl.getInstance();
         User currentUser;
         public EmployerRegistration(User currentUser)
         {
             this.currentUser = currentUser;
             InitializeComponent();
+            phoneBox.Text = "+880";
         }
 
         private void WindowClose_Click(object sender, RoutedEventArgs e)
@@ -45,14 +48,29 @@ namespace JobBoard.WpfApplication
 
         private void EmpRegProceed_Click(object sender, RoutedEventArgs e)
         {
-            Company company = new Company(nameBox.Text,addressbox.Text,countryBox.Text,phoneBox.Text,emailBox.Text,websiteBox.Text, Convert.ToByte(btypeSlider.Value));
-            lrControl.register(company);
-            currentUser.CompanyId = lrControl.getCompanyId(company.Name);
-            currentUser.CompanyName = company.Name;
-            lrControl.register(currentUser);
-            LoginRegister lr = new LoginRegister();
-            lr.Show();
-            this.Hide();
+            if (iehp.isValidEmail(emailBox.Text) && iehp.isPhoneNumber(phoneBox.Text))
+            {
+                Company company = new Company(nameBox.Text, addressbox.Text, countryBox.Text, phoneBox.Text, emailBox.Text, websiteBox.Text, Convert.ToByte(btypeSlider.Value));
+                lrControl.register(company);
+                currentUser.CompanyId = lrControl.getCompanyId(company.Name);
+                currentUser.CompanyName = company.Name;
+                lrControl.register(currentUser);
+                LoginRegister lr = new LoginRegister();
+                lr.Show();
+                this.Hide();
+            }
+            else if (!iehp.isValidEmail(emailBox.Text) && !iehp.isPhoneNumber(phoneBox.Text))
+            {
+                MessageBox.Show("Provide valid Email & Phone Number!");
+            }
+            else if (!iehp.isValidEmail(emailBox.Text))
+            {
+                MessageBox.Show("Provide a valid Email address!");
+            }
+            else if (!iehp.isPhoneNumber(phoneBox.Text))
+            {
+                MessageBox.Show("Provide a valid Phone Number!");
+            }
         }
 
         private void EmployerRegWindow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -64,6 +82,55 @@ namespace JobBoard.WpfApplication
         private void btypeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
+        }
+
+        private void phoneBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!iehp.isValidEmail(emailBox.Text))
+            {
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (iehp.isValidEmail(emailBox.Text))
+            {
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        private void phoneBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!iehp.isPhoneNumber(phoneBox.Text))
+            {
+                phoneBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (iehp.isPhoneNumber(phoneBox.Text))
+            {
+                phoneBox.BorderBrush = new SolidColorBrush(Colors.Green);
+
+            }
+        }
+
+        private void emailBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!iehp.isValidEmail(emailBox.Text))
+            {
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (iehp.isValidEmail(emailBox.Text))
+            {
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        private void emailBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!iehp.isValidEmail(emailBox.Text))
+            {
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+            else if (iehp.isValidEmail(emailBox.Text))
+            {
+                emailBox.BorderBrush = new SolidColorBrush(Colors.Green);
+            }
         }
     }
 }
